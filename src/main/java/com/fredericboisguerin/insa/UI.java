e740abdf3c7697be0c71aa411063940608585de2
binabdul <m.ainolfhatah@gmail.com>
2017-11-17 16:46:55 +0100

    mock CSVTest

--- src/main/java/com/fredericboisguerin/insa/UI.java
+++ src/main/java/com/fredericboisguerin/insa/UI.java
@@ -1,26 +1,28 @@
 package com.fredericboisguerin.insa;
 
+import com.opencsv.CSVWriter;
+
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class UI {
 
-    public static void askInfoForContact() {
+    public static void askInfoForContact(ContactsManager cm) {
         Scanner sc = new Scanner(System.in);
-        ContactsManager cm = new ContactsManager();
         System.out.println("Nom :");
         String name;
         String email;
         String phoneNumber;
 
-        name = sc.next();
+        name = sc.nextLine();
 
         System.out.println("Email[optionnel] ('-' pour passer):");
-        email = sc.next();
+        email = sc.nextLine();
         if(email.equals("-"))
             email = null;
 
         System.out.println("Téléphone[optionnel]('-' pour passer) :");
-        phoneNumber = sc.next();
+        phoneNumber = sc.nextLine();
         if(phoneNumber.equals("-"))
             phoneNumber = null;
 
@@ -33,4 +35,75 @@ public class UI {
         }
     }
 
+    public static void askForListContacts(ContactsManager cm) {
+        System.out.println("Liste des contacts :\n");
+        cm.printAllContacts();
+    }
+
+
+    public static void askForSearchingByName(ContactsManager cm) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer un nom :\n");
+        String name = sc.nextLine();
+        cm.searchContactByName(name);
+    }
+
+    public static void go() {
+        Scanner sc = new Scanner(System.in);
+        ContactsManager cm = new ContactsManager();
+        String res = "";
+
+        while (!res.equals("0")) {
+
+            System.out.println("\nMENU\n" +
+                    "1 - Ajouter un nouveau contact\n" +
+                    "2 - Chercher un contact\n" +
+                    "3 - Afficher tous les contacts\n" +
+                    "4 - Sauvegarder la liste de contact\n" +
+                    "0 - Fermer l'application\n");
+            res = sc.next();
+
+            switch (res) {
+                case "1":
+                    askInfoForContact(cm);
+
+                    break;
+                case "2":
+                    askForSearchingByName(cm);
+                    break;
+                case "3":
+                    askForListContacts(cm);
+                    break;
+                case "4":
+                    askForSaveInCSV(cm);
+
+                    break;
+                case "0":
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Erreur : valeur incorrecte. Veuillez réessayer");
+            }
+        }
+    }
+
+
+    public static void askForSaveInCSV(ContactsManager cm) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez le nom du fichier : ");
+        String nameFile = sc.next();
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter(nameFile + ".csv"), '\t');
+            String[] contacts = cm.getAllContactsForCSV();
+            for (int i = 0; i < contacts.length; i++) {
+                String[] entries = contacts[i].split(",");
+                writer.writeNext(entries);
+            }
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
--- src/test/java/com/fredericboisguerin/insa/GUITest.java
+++ src/test/java/com/fredericboisguerin/insa/GUITest.java
@@ -5,11 +5,9 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 import java.io.ByteArrayInputStream;
-import java.util.List;
 
 public class GUITest {
 
-    private List mockedList;
     private ContactsManager mockedContactsManager;
 
     @Before
@@ -18,12 +16,49 @@ public class GUITest {
     }
 
     @Test
-    public void add_one_contact() throws InvalidEmailException, InvalidContactNameException {
-        String input = "Tehema - -";
+    public void add_one_contact_without_email_nor_phone_number() throws InvalidEmailException, InvalidContactNameException {
+        String input = "Tehema\n-\n-";
         System.setIn(new ByteArrayInputStream(input.getBytes())); // on simule les entrées utilisateurs (on contrôle le flux d'entrée)
-        UI.askInfoForContact(); // affiche "entrer le nom du contact
+        UI.askInfoForContact(mockedContactsManager); // affiche "entrer le nom du contact
         verify(mockedContactsManager).addContact("Tehema",null,null);
+    }
 
+    @Test
+    public void add_one_contact_without_email() throws InvalidContactNameException,InvalidEmailException {
+        String input = "Tehema\n-\n0102030405";
+        System.setIn(new ByteArrayInputStream(input.getBytes())); // on simule les entrées utilisateurs (on contrôle le flux d'entrée)
+        UI.askInfoForContact(mockedContactsManager); // affiche "entrer le nom du contact
+        verify(mockedContactsManager).addContact("Tehema",null,"0102030405");
+    }
+
+    @Test
+    public void add_one_contact_without_phone_number() throws InvalidContactNameException , InvalidEmailException{
+        String input = "Tehema\ntehema@gmail.com\n-";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        UI.askInfoForContact(mockedContactsManager);
+        verify(mockedContactsManager).addContact("Tehema","tehema@gmail.com",null);
+    }
+
+    @Test
+    public void list_contacts() throws InvalidContactNameException,InvalidEmailException {
+        UI.askForListContacts(mockedContactsManager);
+        verify(mockedContactsManager).printAllContacts();
+    }
+
+    @Test
+    public void search_contact_by_name() throws InvalidContactNameException,InvalidEmailException {
+        String input = "Tehema";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        UI.askForSearchingByName(mockedContactsManager);
+        verify(mockedContactsManager).searchContactByName("Tehema");
+    }
+
+    @Test
+    public void save_in_csv() throws InvalidContactNameException,InvalidEmailException {
+        String input = "contactTest";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        UI.askForSaveInCSV(mockedContactsManager);
+        verify(mockedContactsManager).searchContactByName("Tehema");
     }
 
 }
